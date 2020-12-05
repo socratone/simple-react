dba53f4f7227271988a3027e2e491495892783c8
socratone <gim2origin@gmail.com>
2020-12-06 01:02:25 +0900

    코드 간략히 수정

--- src/App.js
+++ src/App.js
@@ -2,9 +2,7 @@ import React from 'react';
 import styles from './App.scss';
 
 const App = () => {
-  return (
-    <div className={styles.helloWorld}>hello world</div>
-  );
+  return <div className={styles.helloWorld}>hello world</div>;
 }
 
 export default App;

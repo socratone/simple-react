3f4204d3466dc00e030ddcb9a82311d9cfe7d702
socratone <gim2origin@gmail.com>
2020-12-05 22:54:11 +0900

    First commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './App.scss';
+
+const App = () => {
+  return (
+    <div className={styles.helloWorld}>hello world</div>
+  );
+}
+
+export default App;
